cbd9ab3ab9a34cb747077b2dbfaa9f5100b4c820
Brian Brinkman <brian@streamcreative.com>
2023-09-01 20:26:53 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,30 +1,26 @@
 import streamlit as st
 import openai
 
-# Initialize Streamlit
-st.title("OpenAI API Key Test with Davinci Model")
+# Set the OpenAI API key
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-# Load the API key from Streamlit's secrets
-if 'OPENAI_API_KEY' in st.secrets:
-    st.sidebar.success('API key successfully loaded from secrets!')
-else:
-    st.sidebar.error('Failed to load API key from secrets.')
-
-# Test the OpenAI API with a simple completion using davinci model
 def test_openai_api():
     try:
         response = openai.Completion.create(
-          engine="davinci",
-          prompt="Translate the following English text to French: 'Hello World'",
-          max_tokens=60
+            engine="davinci",
+            prompt="Translate the following English text to French: 'Hello, how are you?'",
+            max_tokens=60
         )
         return response.choices[0].text.strip()
     except Exception as e:
         return str(e)
 
-# Display the result or error
+st.title("OpenAI API Key Test with Davinci Model")
+
 result = test_openai_api()
+
 if "Translate the following English text to French:" in result:
-    st.success(f"API is working correctly! Response: {result}")
+    st.write("API test passed!")
+    st.write(result)
 else:
-    st.error(f"API test failed. Error: {result}")
+    st.write("API test failed. Error:", result)
